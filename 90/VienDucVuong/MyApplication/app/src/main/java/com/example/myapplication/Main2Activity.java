0f0918bc71536b1466fd4b4ae07ad85ed668c16b
doubleVz2603 <vuongthhthkht@gmail.com>
2020-10-03 18:34:32 +0700

    VienDucVuong

---
+++ 90/VienDucVuong/MyApplication/app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,19 @@
+package com.example.myapplication;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trung);
+
+    }
+
+}
--- 90/VienDucVuong/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ 90/VienDucVuong/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton btnFirst;
+    ImageButton btnSecond;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnFirst = (ImageButton) findViewById(R.id.imageButton_edu);
+        btnFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, layout2.class);
+                startActivity(intent);
+            }
+        });
+
+
+        btnSecond = (ImageButton) findViewById(R.id.imageButton_des);
+        btnSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, layout3.class);
+                startActivity(intent);
+            }
+        });
     }
+    public void batdau(View view) {
+        Intent i = new Intent(this,Main2Activity.class);
+        startActivity(i);
+    }
+
+
 }
+

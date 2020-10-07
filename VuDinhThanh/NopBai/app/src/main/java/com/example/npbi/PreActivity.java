99a2c8c8c9897fdb2871afeeb4d97815ed3c8dd7
VUTHANHUET <71000712+VUTHANHUET@users.noreply.github.com>
2020-10-07 11:00:49 +0700

    update

--- VuDinhThanh/NopBai/app/src/main/java/com/example/npbi/PreActivity.java
+++ VuDinhThanh/NopBai/app/src/main/java/com/example/npbi/PreActivity.java
@@ -3,15 +3,26 @@ package com.example.npbi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PreActivity extends AppCompatActivity {
     ActionBar actionBar;
+    Button khoahoccoban;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre);
+        khoahoccoban = findViewById(R.id.khoahoc);
         actionBar = getSupportActionBar();
         actionBar.hide();
+        khoahoccoban.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PreActivity.this, MainActivity.class));
+            }
+        });
     }
 }

186e64451563333cafa05d0362919f60728e1d96
congminh1809 <congminh1809@gmail.com>
2020-10-14 13:38:29 +0700

    DuoiHinhBatChu

---
+++ DaoCongMinh/DuoiHinhBatChu/app/src/main/java/adapter/DapAnAdapter.java
@@ -0,0 +1,39 @@
+package adapter;
+
+import android.content.Context;
+import android.e.duoihinhbatchu.R;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DapAnAdapter extends ArrayAdapter<String> {
+    private Context myCt;
+    private ArrayList<String> arr;
+    public DapAnAdapter(@NonNull Context context, int resource, @NonNull List<String> objects) {
+        super(context, resource, objects);
+        this.myCt = context;
+        this.arr = new ArrayList<>(objects);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater)myCt.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_cau_tra_loi, null);
+        }
+
+        TextView txtCauTraLoi = convertView.findViewById(R.id.txtCauTraLoi);
+        txtCauTraLoi.setText(this.arr.get(position));
+        return convertView;
+    }
+}

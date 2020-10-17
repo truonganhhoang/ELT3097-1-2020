c3729b06f61508a2ba367b7071f62889e35d9105
LE-VAN-HUNG <65001663+LE-VAN-HUNG@users.noreply.github.com>
2020-10-17 21:02:37 +0700

    submit

--- 08/Duolingo/app/src/main/java/com/example/duolingo/MainActivity.java
+++ 08/Duolingo/app/src/main/java/com/example/duolingo/MainActivity.java
@@ -11,14 +11,14 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button back;
+    ImageButton Back;
     Button Ans1;
     Button Ans2;
     Button Ans3;
     Button Ans4;
-    Button button5;
-    Button button6;
-    RelativeLayout layout1;
+    Button Submit;
+    Button Continue;
+    RelativeLayout layoutCheck;
     TextView textView3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +26,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setupUIView();
 
-    back.setOnClickListener(new View.OnClickListener() {
+    Back.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
         startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
     });
-    button6.setOnClickListener(new View.OnClickListener() {
+    Continue.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
         startActivity(new Intent(MainActivity.this, CardActivity.class));
@@ -43,19 +43,19 @@ public class MainActivity extends AppCompatActivity {
         Ans2.setOnClickListener(mListener);
         Ans3.setOnClickListener(mListener);
         Ans4.setOnClickListener(mListener);
-        button5.setOnClickListener(mListener);
+        Submit.setOnClickListener(mListener);
 
     }
     private void setupUIView(){
-        back = findViewById(R.id.back);
+        Back = findViewById(R.id.back);
         Ans1 = findViewById(R.id.ans1);
         Ans2 = findViewById(R.id.ans2);
         Ans3 = findViewById(R.id.ans3);
         Ans4 = findViewById(R.id.ans4);
-        button5 = findViewById(R.id.submit);
-        button6 = findViewById(R.id.kiemtra);
-        layout1 = findViewById(R.id.layout1);
-        textView3 = findViewById(R.id.textView2);
+        Submit = findViewById(R.id.submit);
+        Continue = findViewById(R.id.kiemtra);
+        layoutCheck = findViewById(R.id.layout1);
+        textView3 = findViewById(R.id.textView3);
     }
     View.OnClickListener mListener = new View.OnClickListener() {
         @Override
@@ -63,52 +63,52 @@ public class MainActivity extends AppCompatActivity {
             int Id = v.getId();
             switch (Id) {
                 case R.id.ans1:
-                    button5.setEnabled(true);
-                    button5.setBackgroundResource(R.drawable.custom_submit);
+                    Submit.setEnabled(true);
+                    Submit.setBackgroundResource(R.drawable.custom_submit);
                     Ans1.setSelected(true);
                     Ans2.setSelected(false);
                     Ans3.setSelected(false);
                     Ans4.setSelected(false);
                     break;
                 case R.id.ans2:
-                    button5.setEnabled(true);
+                    Submit.setEnabled(true);
                     Ans2.setSelectAllOnFocus(true);
-                    button5.setBackgroundResource(R.drawable.custom_submit);
+                    Submit.setBackgroundResource(R.drawable.custom_submit);
                     Ans2.setSelected(true);
                     Ans1.setSelected(false);
                     Ans3.setSelected(false);
                     Ans4.setSelected(false);
                     break;
                 case R.id.ans3:
-                    button5.setEnabled(true);
+                    Submit.setEnabled(true);
                     Ans3.setSelectAllOnFocus(true);
-                    button5.setBackgroundResource(R.drawable.custom_submit);
+                    Submit.setBackgroundResource(R.drawable.custom_submit);
                     Ans3.setSelected(true);
                     Ans1.setSelected(false);
                     Ans4.setSelected(false);
                     Ans2.setSelected(false);
                     break;
                 case R.id.ans4:
-                    button5.setEnabled(true);
+                    Submit.setEnabled(true);
                     Ans4.setSelected(true);
                     Ans1.setSelected(false);
                     Ans3.setSelected(false);
                     Ans2.setSelected(false);
-                    button5.setBackgroundResource(R.drawable.custom_submit);
+                    Submit.setBackgroundResource(R.drawable.custom_submit);
 
                     break;
                 case R.id.submit:
                     if(Ans4.isSelected()) {
-                        button5.setBackgroundResource(R.drawable.custom_true);
-                        button5.setText("Giỏi lắm!");
-                        layout1.setVisibility(layout1.VISIBLE);
-                        button5.setVisibility(View.INVISIBLE);
+                        Submit.setBackgroundResource(R.drawable.custom_true);
+                        textView3.setText("Giỏi lắm!");
+                        layoutCheck.setVisibility(layoutCheck.VISIBLE);
+                        Submit.setVisibility(View.INVISIBLE);
                     }
                     else {
-                        layout1.setVisibility(layout1.VISIBLE);
-                        layout1.setBackgroundResource(R.drawable.custom_false);
-                        button6.setBackgroundResource(R.drawable.custom_btnfalse);
-                        button5.setVisibility(View.INVISIBLE);
+                        layoutCheck.setVisibility(layoutCheck.VISIBLE);
+                        layoutCheck.setBackgroundResource(R.drawable.custom_false);
+                        Continue.setBackgroundResource(R.drawable.custom_btnfalse);
+                        Submit.setVisibility(View.INVISIBLE);
                         textView3.setText("Trả lời đúng: \ntea ");
                         textView3.setTextColor(getResources().getColor(R.color.Recolor));
                     }

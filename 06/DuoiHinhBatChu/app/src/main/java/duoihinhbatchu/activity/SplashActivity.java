406b36d3d403082afd0ff96f8fcd1169358f7ae6
congminh1809 <congminh1809@gmail.com>
2020-11-09 21:52:19 +0700

    Update SplashActivity.java

--- 06/DuoiHinhBatChu/app/src/main/java/duoihinhbatchu/activity/SplashActivity.java
+++ 06/DuoiHinhBatChu/app/src/main/java/duoihinhbatchu/activity/SplashActivity.java
@@ -2,15 +2,9 @@ package nhom6.com.duoihinhbatchu.activity;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.WindowManager;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.InterstitialAd;
-import com.google.android.gms.ads.MobileAds;
+
 import duoihinhbatchu.MainActivity;
-import nhom6.com.duoihinhbatchu.R;
+
 
 public class SplashActivity extends AppCompatActivity {
     private InterstitialAd interstitialAd;
@@ -36,22 +30,4 @@ public class SplashActivity extends AppCompatActivity {
         initViews();
     }
 
-    private void startMain() {
-        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
 
-    private void initViews() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (interstitialAd.isLoaded()) {
-                    interstitialAd.show();
-                } else
-
-                    startMain();
-            }
-        }, 2000);
-    }
-}

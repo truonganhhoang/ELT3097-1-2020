d047c72c929e13babc24c198c561f1b380c18d99
tien-thu <dothu010920@gmail.com>
2020-12-09 00:54:40 +0700

    uplate

---
+++ nhom96/app/src/main/java/com/example/nhom96/PracticeFragment/datamodel.java
@@ -0,0 +1,18 @@
+package com.example.nhom96.PracticeFragment;
+
+public class datamodel {
+    int image;
+
+    public datamodel(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+}
